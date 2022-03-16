0eb2a95765d26452f22da7687c4b20bdcf0d0480
jesse.hunt <jh.trumpet37@gmail.com>
2022-03-16 18:23:38 +0100

    - adding templates - updated pipenv stuff

--- sample_project/sample_project/settings.py
+++ sample_project/sample_project/settings.py
@@ -10,9 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from docutils.nodes import option_string
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -57,7 +60,7 @@ ROOT_URLCONF = "sample_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
